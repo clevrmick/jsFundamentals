3c5a9052033253a18e890bbe12ccdcb4d21b56f6
clevrmick <clevrmick@gmail.com>
2021-04-24 22:42:51 +0100

    nested loop

--- js/forLoop.js
+++ js/forLoop.js
@@ -6,6 +6,74 @@ for (let i = 0; i <= 10; i++) {
 
 // infinig loop
 
-for (let i = 1; i !== 20; i += 6) {
-    console.log('infit loop');
+// for (let i = 1; i !== 20; i += 6) {
+//     // console.log('infit loop');
+// }
+
+let examScores = [22, 45, 88, 54, 92, 90];
+
+for (let i = 0; i < examScores.length; i++) {
+    console.log(examScores[i], i);
 }
+
+const myStudent = [
+    {
+        firstName: 'Zeus',
+        grade    : 86
+    },
+    {
+        firstName: 'Artemis',
+        grade    : 97
+    },
+    {
+        firstName: 'Hera',
+        grade    : 72
+    },
+    {
+        firstName: 'Apolo',
+        grade    : 90
+    },
+];
+
+for (let i = 0; i < myStudent.length; i++) {
+    let student = myStudent[i];
+    console.log(`${student.firstName} scored ${student.grade}`);
+}
+
+// looping opposite from the end
+
+const word = 'bles';
+let reversedWord = '';
+
+for (let i = word.length -1; i > 0; i--) {
+    //  console.log(i)
+    console.log(reversedWord += word[i]);
+}
+
+// lets average the student scores 
+
+const myStudentResult = [
+    {
+        firstName: 'Zeus',
+        grade    : 86
+    },
+    {
+        firstName: 'Artemis',
+        grade    : 97
+    },
+    {
+        firstName: 'Hera',
+        grade    : 72
+    },
+    {
+        firstName: 'Apolo',
+        grade    : 90
+    },
+];
+
+let total = 0;
+for (let j = 0; j < myStudentResult.length; j++) {
+    let studentGrad = myStudentResult[j];
+    total += studentGrad.grade;
+}
+console.log(total/myStudentResult.length);
---
+++ js/whileLoop.js
@@ -0,0 +1,15 @@
+// A while loop continues to run as long as its tests condition is true
+// example in for loop 
+
+for (let i = 0; i < 5; i++) {
+    console.log(`This is loop in forLoop ${i}`);
+}
+
+// example in while loop
+
+let j = 0;
+
+while (j < 5) {
+    console.log(`This is loop in whileLoop ${j}`);
+    j++;
+}
