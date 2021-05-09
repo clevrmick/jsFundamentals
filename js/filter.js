ab4d809e263178bebb5ead8be02f8bfb7c606cd7
clevrmick <clevrmick@gmail.com>
2021-05-09 16:59:34 +0100

    completed filter

--- js/filter.js
+++ js/filter.js
@@ -5,7 +5,7 @@
  * 
  * 
  */
-
+// filter for odd and even Numbers and bigger Numbers (numbers bigger than 50 )
 const numbers = [88, 34, 57, 67, 54, 109, 102, 32, 9];
 
     // doing it with function
@@ -35,9 +35,108 @@ const numbers = [88, 34, 57, 67, 54, 109, 102, 32, 9];
 
     //  doing it with filter
 
-const oddNumbers = numbers.filter((number) => number % 2 === 1);
-const evenNumber = numbers.filter((number) => number % 2 === 0);
+const oddNumbers = numbers.filter((number) => number % 2 === 1); // filter odd numbers. non divisible by 2
+const evenNumber = numbers.filter((number) => number % 2 === 0); // filter even numbers. divisible by 2
+const bigNumber = numbers.filter((number) => number > 50); // filter number larger than 50
 
-console.log(oddNumbers);
-console.log(evenNumber);
+console.log(oddNumbers); // filter odd numbers. non divisible by 2
+console.log(evenNumber); // filter even numbers. divisible by 2
+console.log(bigNumber); // filter number larger than 50
 console.log(numbers); // the original arry of Numbers is not change it imutable 
+
+const books = [
+    {
+        title: 'Good Omens',
+        authors: ['Terry pratchett', 'Neil Gaiman'],
+        rating: 4.25,
+        generes: ['fiction', 'fantasy']
+    },
+    {
+        title: 'Changing My Mind',
+        authors: ['Zaddie Smith'],
+        rating: 3.83,
+        generes: ['nonfiction', 'essays']
+    },
+    {
+        title: 'Bone: The complete edition',
+        authors: ['Jeff Smith'],
+        rating: 4.42,
+        generes: ['fiction', 'graphc', 'fantasy']
+        
+    },
+    {
+        title: 'American gods',
+        authors: ['Neil Gaiman'],
+        rating: 4.11,
+        generes: ['fiction', 'fantasy']
+    },
+    {
+        title: 'A Gentle man in Moscow',
+        authors: ['Amor Towles'],
+        rating: 4.36,
+        generes: ['fiction', 'historical fiction', 'fantasy']
+    },
+    {
+        title: 'The Name of the wind',
+        authors: ['Patrick Rothfuss'],
+        rating: 4.54,
+        generes: ['fiction', 'fantasy']
+    },
+    {
+        title: 'The Oversotory',
+        authors: ['Richard Powers'],
+        rating: 4.19,
+        generes: ['fiction', 'short stories']
+    },
+    {
+        title: 'The Way Of Kings',
+        authors: ['Brandon Sanderson'],
+        rating: 4.65,
+        generes: ['fiction', 'Epic']
+    },
+    {
+        title: 'Lord Of The Flies',
+        authors: ['William Golding'],
+        rating: 3.67,
+        generes: ['Fiction']
+    }
+
+
+]
+
+console.log(books);
+
+// lets filter books that the rating is higher than 4.3
+
+const goodBooks = books.filter((book) => book.rating > 4.3); // filter book that its rating is higher tha 4.3 
+console.log(goodBooks);
+
+// let's filter for book that contains a genere of fantasy
+
+const fantasyBooks = books.filter((book) => ( // filter for book that includes fantasy
+    book.generes.includes('fantasy')
+))
+
+console.log(fantasyBooks);
+
+// let's filter for book that either contains short stories or essays 
+
+const shortForms = books.filter((book) => (
+    book.generes.includes('short stories') || book.generes.includes('essays')
+))
+
+console.log(shortForms);
+
+// let create a search key work that start with the
+
+
+const query = 'the';
+
+const result = books.filter((book) => {
+    // return book.title.toLowercase().includes(query.toLowerCase())
+
+    const title = book.title.toLowerCase();
+    return title.includes(query.toLowerCase());
+})
+
+console.log(result);
